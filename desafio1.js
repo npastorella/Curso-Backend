2dbcd30230aba46ec3be9e9d6621b13177e1f597
Nicolas Pastorella <nicolas.pastorella@gmail.com>
2023-04-11 15:11:26 -0300

    inicio desafio 1

---
+++ desafio1.js
@@ -0,0 +1,33 @@
+/* function Products() {
+    this.products = []; // Creamos el atributo "products" como un arreglo vacío
+
+    // Creamos una función para agregar un nuevo producto al arreglo
+    this.addProduct = function(title, description, price, thumbnail, id, stock) {
+      let newProduct = {
+        title,
+        description,
+        price,
+        thumbnail,
+        id,
+        stock
+      };
+      this.products.push(miProducto);
+    };
+}
+
+
+let miProducto = new Products();
+miProducto.addProduct("iPhone", "El mejor teléfono inteligente", 1000, "img/iphone.jpg", 1, 10);
+console.log("Lista de productos cargados: " + miProducto); */
+
+class ProductManager{
+
+    constructor(){
+        this.productos = [];
+    }
+
+
+
+
+    
+}
